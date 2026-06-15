6ef8e523ae81038068d928f198e273e738f88591
shubham-pasricha1096 <shubhampasricha1096@gmail.com>
2026-06-15 23:43:58 +0530

    fix: Correct ObjectId conversion for Customer model

--- backend/app/crud/crud_customer.py
+++ backend/app/crud/crud_customer.py
@@ -8,20 +8,20 @@ async def get_or_create_customer(customer: CustomerCreate) -> Customer:
     db = get_database()
     existing_customer = await db.customers.find_one({"phone_number": customer.phone_number})
     if existing_customer:
-        existing_customer["id"] = str(existing_customer["_id"])
+        existing_customer["_id"] = str(existing_customer["_id"])
         return Customer(**existing_customer)
     
     new_customer_data = customer.model_dump()
     result = await db.customers.insert_one(new_customer_data)
     created_customer = await db.customers.find_one({"_id": result.inserted_id})
-    created_customer["id"] = str(created_customer["_id"])
+    created_customer["_id"] = str(created_customer["_id"])
     return Customer(**created_customer)
 
 async def get_customer_by_id(customer_id: str) -> Optional[Customer]:
     db = get_database()
     customer = await db.customers.find_one({"_id": ObjectId(customer_id)})
     if customer:
-        customer["id"] = str(customer["_id"])
+        customer["_id"] = str(customer["_id"])
         return Customer(**customer)
     return None
 
@@ -29,7 +29,7 @@ async def get_customer_by_phone(phone_number: str) -> Optional[Customer]:
     db = get_database()
     customer = await db.customers.find_one({"phone_number": phone_number})
     if customer:
-        customer["id"] = str(customer["_id"])
+        customer["_id"] = str(customer["_id"])
         return Customer(**customer)
     return None
 
